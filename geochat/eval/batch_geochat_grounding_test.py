2fd16e0a4011e34845ffafd37236c8c821096337
BinSpa <luogu1024116558@gmail.com>
2024-12-06 18:47:39 +0800

    changes

--- geochat/eval/batch_geochat_grounding_test.py
+++ geochat/eval/batch_geochat_grounding_test.py
@@ -86,8 +86,9 @@ def eval_model(args):
             image_file = anns[j]['filename']
 
         
-            qs = "[grounding]" + "Give me the location of " + anns[j]['objects'][0]['description'] + ". Please return the answer in the format of [xmin, ymin, xmax, ymax]."
-
+            qs = "Give bounding box for " + anns[j]['objects'][0]['description'] + "."
+            print(qs)
+            
             if model.config.mm_use_im_start_end:
                 qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
             else:
