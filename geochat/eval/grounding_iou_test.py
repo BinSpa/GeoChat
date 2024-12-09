5110a3ead1560bafe2f27c8291edd9caa116de48
BinSpa <luogu1024116558@gmail.com>
2024-12-09 21:07:47 +0800

    changes

--- geochat/eval/grounding_iou_test.py
+++ geochat/eval/grounding_iou_test.py
@@ -79,7 +79,8 @@ def calculate_rotated_iou(pred_bbox, pred_angle, gt_bbox):
 def parse_answer(answer):
     answer = answer.strip()
     if is_valide_answer_format(answer):
-        bbox_part, angle_part = answer.split('|')[0], answer.split('|')[1]
+        answer = answer.strip('{}')
+        bbox_part, angle_part = answer.split('|')
         bbox_values = [int(v.strip('<>')) for v in bbox_part.split('><')]
         angle = int(angle_part.strip('<>'))
         # 返回 (xmin, ymin, xmax, ymax), angle
